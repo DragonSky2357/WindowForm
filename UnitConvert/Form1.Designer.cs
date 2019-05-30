df08a80800cdac64a4a3005f616eacffc642de81
DragonSky2357 <qkrdydals327@gmail.com>
2019-05-31 00:38:12 +0900

    All of UnitConvert Source Code

---
+++ UnitConvert/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace UnitConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtByte = new System.Windows.Forms.TextBox();
+            this.lblBit = new System.Windows.Forms.Label();
+            this.txtBit = new System.Windows.Forms.TextBox();
+            this.lblKB = new System.Windows.Forms.Label();
+            this.lblMB = new System.Windows.Forms.Label();
+            this.lblGB = new System.Windows.Forms.Label();
+            this.lblTB = new System.Windows.Forms.Label();
+            this.lblPB = new System.Windows.Forms.Label();
+            this.lblByte = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.txtKb = new System.Windows.Forms.TextBox();
+            this.txtMb = new System.Windows.Forms.TextBox();
+            this.txtGb = new System.Windows.Forms.TextBox();
+            this.txtTb = new System.Windows.Forms.TextBox();
+            this.txtPb = new System.Windows.Forms.TextBox();
+            this.lblMill = new System.Windows.Forms.Label();
+            this.txtMs = new System.Windows.Forms.TextBox();
+            this.txtUs = new System.Windows.Forms.TextBox();
+            this.txtNs = new System.Windows.Forms.TextBox();
+            this.txtPs = new System.Windows.Forms.TextBox();
+            this.txtFs = new System.Windows.Forms.TextBox();
+            this.txtAs = new System.Windows.Forms.TextBox();
+            this.lblNano = new System.Windows.Forms.Label();
+            this.lblFemto = new System.Windows.Forms.Label();
+            this.lblMicro = new System.Windows.Forms.Label();
+            this.lblPico = new System.Windows.Forms.Label();
+            this.lblAtto = new System.Windows.Forms.Label();
+            this.txtBinaryNumber = new System.Windows.Forms.TextBox();
+            this.lblBinaryNumber = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtByte
+            // 
+            this.txtByte.Location = new System.Drawing.Point(51, 12);
+            this.txtByte.Name = "txtByte";
+            this.txtByte.Size = new System.Drawing.Size(136, 21);
+            this.txtByte.TabIndex = 0;
+            this.txtByte.TextChanged += new System.EventHandler(this.txtByte_TextChanged);
+            // 
+            // lblBit
+            // 
+            this.lblBit.AutoSize = true;
+            this.lblBit.Location = new System.Drawing.Point(21, 70);
+            this.lblBit.Name = "lblBit";
+            this.lblBit.Size = new System.Drawing.Size(24, 12);
+            this.lblBit.TabIndex = 1;
+            this.lblBit.Text = "BIT";
+            // 
+            // txtBit
+            // 
+            this.txtBit.Location = new System.Drawing.Point(51, 67);
+            this.txtBit.Name = "txtBit";
+            this.txtBit.Size = new System.Drawing.Size(136, 21);
+            this.txtBit.TabIndex = 4;
+            // 
+            // lblKB
+            // 
+            this.lblKB.AutoSize = true;
+            this.lblKB.Location = new System.Drawing.Point(21, 105);
+            this.lblKB.Name = "lblKB";
+            this.lblKB.Size = new System.Drawing.Size(20, 12);
+            this.lblKB.TabIndex = 6;
+            this.lblKB.Text = "Kb";
+            // 
+            // lblMB
+            // 
+            this.lblMB.AutoSize = true;
+            this.lblMB.Location = new System.Drawing.Point(21, 140);
+            this.lblMB.Name = "lblMB";
+            this.lblMB.Size = new System.Drawing.Size(23, 12);
+            this.lblMB.TabIndex = 1;
+            this.lblMB.Text = "Mb";
+            // 
+            // lblGB
+            // 
+            this.lblGB.AutoSize = true;
+            this.lblGB.Location = new System.Drawing.Point(21, 175);
+            this.lblGB.Name = "lblGB";
+            this.lblGB.Size = new System.Drawing.Size(21, 12);
+            this.lblGB.TabIndex = 6;
+            this.lblGB.Text = "Gb";
+            // 
+            // lblTB
+            // 
+            this.lblTB.AutoSize = true;
+            this.lblTB.Location = new System.Drawing.Point(21, 210);
+            this.lblTB.Name = "lblTB";
+            this.lblTB.Size = new System.Drawing.Size(20, 12);
+            this.lblTB.TabIndex = 1;
+            this.lblTB.Text = "Tb";
+            // 
+            // lblPB
+            // 
+            this.lblPB.AutoSize = true;
+            this.lblPB.Location = new System.Drawing.Point(21, 245);
+            this.lblPB.Name = "lblPB";
+            this.lblPB.Size = new System.Drawing.Size(20, 12);
+            this.lblPB.TabIndex = 6;
+            this.lblPB.Text = "Pb";
+            // 
+            // lblByte
+            // 
+            this.lblByte.AutoSize = true;
+            this.lblByte.Font = new System.Drawing.Font("Gulim", 9F);
+            this.lblByte.Location = new System.Drawing.Point(11, 16);
+            this.lblByte.Name = "lblByte";
+            this.lblByte.Size = new System.Drawing.Size(30, 12);
+            this.lblByte.TabIndex = 7;
+            this.lblByte.Text = "Byte";
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.AutoSize = true;
+            this.lblSecond.Location = new System.Drawing.Point(198, 15);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(27, 12);
+            this.lblSecond.TabIndex = 8;
+            this.lblSecond.Text = "Sec";
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(228, 12);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(136, 21);
+            this.txtSecond.TabIndex = 9;
+            this.txtSecond.TextChanged += new System.EventHandler(this.txtSecond_TextChanged);
+            // 
+            // txtKb
+            // 
+            this.txtKb.Location = new System.Drawing.Point(51, 102);
+            this.txtKb.Name = "txtKb";
+            this.txtKb.Size = new System.Drawing.Size(136, 21);
+            this.txtKb.TabIndex = 4;
+            // 
+            // txtMb
+            // 
+            this.txtMb.Location = new System.Drawing.Point(51, 137);
+            this.txtMb.Name = "txtMb";
+            this.txtMb.Size = new System.Drawing.Size(136, 21);
+            this.txtMb.TabIndex = 4;
+            // 
+            // txtGb
+            // 
+            this.txtGb.Location = new System.Drawing.Point(51, 172);
+            this.txtGb.Name = "txtGb";
+            this.txtGb.Size = new System.Drawing.Size(136, 21);
+            this.txtGb.TabIndex = 4;
+            // 
+            // txtTb
+            // 
+            this.txtTb.Location = new System.Drawing.Point(51, 207);
+            this.txtTb.Name = "txtTb";
+            this.txtTb.Size = new System.Drawing.Size(136, 21);
+            this.txtTb.TabIndex = 4;
+            // 
+            // txtPb
+            // 
+            this.txtPb.Location = new System.Drawing.Point(51, 242);
+            this.txtPb.Name = "txtPb";
+            this.txtPb.Size = new System.Drawing.Size(136, 21);
+            this.txtPb.TabIndex = 4;
+            // 
+            // lblMill
+            // 
+            this.lblMill.AutoSize = true;
+            this.lblMill.Location = new System.Drawing.Point(198, 70);
+            this.lblMill.Name = "lblMill";
+            this.lblMill.Size = new System.Drawing.Size(23, 12);
+            this.lblMill.TabIndex = 1;
+            this.lblMill.Text = "Ms";
+            // 
+            // txtMs
+            // 
+            this.txtMs.Location = new System.Drawing.Point(228, 67);
+            this.txtMs.Name = "txtMs";
+            this.txtMs.Size = new System.Drawing.Size(136, 21);
+            this.txtMs.TabIndex = 4;
+            // 
+            // txtUs
+            // 
+            this.txtUs.Location = new System.Drawing.Point(228, 102);
+            this.txtUs.Name = "txtUs";
+            this.txtUs.Size = new System.Drawing.Size(136, 21);
+            this.txtUs.TabIndex = 4;
+            // 
+            // txtNs
+            // 
+            this.txtNs.Location = new System.Drawing.Point(228, 137);
+            this.txtNs.Name = "txtNs";
+            this.txtNs.Size = new System.Drawing.Size(136, 21);
+            this.txtNs.TabIndex = 4;
+            // 
+            // txtPs
+            // 
+            this.txtPs.Location = new System.Drawing.Point(228, 172);
+            this.txtPs.Name = "txtPs";
+            this.txtPs.Size = new System.Drawing.Size(136, 21);
+            this.txtPs.TabIndex = 4;
+            // 
+            // txtFs
+            // 
+            this.txtFs.Location = new System.Drawing.Point(228, 207);
+            this.txtFs.Name = "txtFs";
+            this.txtFs.Size = new System.Drawing.Size(136, 21);
+            this.txtFs.TabIndex = 4;
+            // 
+            // txtAs
+            // 
+            this.txtAs.Location = new System.Drawing.Point(228, 242);
+            this.txtAs.Name = "txtAs";
+            this.txtAs.Size = new System.Drawing.Size(136, 21);
+            this.txtAs.TabIndex = 4;
+            // 
+            // lblNano
+            // 
+            this.lblNano.AutoSize = true;
+            this.lblNano.Location = new System.Drawing.Point(198, 140);
+            this.lblNano.Name = "lblNano";
+            this.lblNano.Size = new System.Drawing.Size(21, 12);
+            this.lblNano.TabIndex = 1;
+            this.lblNano.Text = "Ns";
+            // 
+            // lblFemto
+            // 
+            this.lblFemto.AutoSize = true;
+            this.lblFemto.Location = new System.Drawing.Point(198, 210);
+            this.lblFemto.Name = "lblFemto";
+            this.lblFemto.Size = new System.Drawing.Size(19, 12);
+            this.lblFemto.TabIndex = 1;
+            this.lblFemto.Text = "Fs";
+            // 
+            // lblMicro
+            // 
+            this.lblMicro.AutoSize = true;
+            this.lblMicro.Location = new System.Drawing.Point(198, 105);
+            this.lblMicro.Name = "lblMicro";
+            this.lblMicro.Size = new System.Drawing.Size(20, 12);
+            this.lblMicro.TabIndex = 6;
+            this.lblMicro.Text = "Us";
+            // 
+            // lblPico
+            // 
+            this.lblPico.AutoSize = true;
+            this.lblPico.Location = new System.Drawing.Point(198, 175);
+            this.lblPico.Name = "lblPico";
+            this.lblPico.Size = new System.Drawing.Size(20, 12);
+            this.lblPico.TabIndex = 6;
+            this.lblPico.Text = "Ps";
+            // 
+            // lblAtto
+            // 
+            this.lblAtto.AutoSize = true;
+            this.lblAtto.Location = new System.Drawing.Point(198, 245);
+            this.lblAtto.Name = "lblAtto";
+            this.lblAtto.Size = new System.Drawing.Size(20, 12);
+            this.lblAtto.TabIndex = 6;
+            this.lblAtto.Text = "As";
+            // 
+            // txtBinaryNumber
+            // 
+            this.txtBinaryNumber.Location = new System.Drawing.Point(51, 269);
+            this.txtBinaryNumber.Multiline = true;
+            this.txtBinaryNumber.Name = "txtBinaryNumber";
+            this.txtBinaryNumber.Size = new System.Drawing.Size(313, 35);
+            this.txtBinaryNumber.TabIndex = 10;
+            // 
+            // lblBinaryNumber
+            // 
+            this.lblBinaryNumber.AutoSize = true;
+            this.lblBinaryNumber.Font = new System.Drawing.Font("Gulim", 7F);
+            this.lblBinaryNumber.Location = new System.Drawing.Point(3, 275);
+            this.lblBinaryNumber.Name = "lblBinaryNumber";
+            this.lblBinaryNumber.Size = new System.Drawing.Size(42, 20);
+            this.lblBinaryNumber.TabIndex = 11;
+            this.lblBinaryNumber.Text = "Binary\r\nNumber";
+            this.lblBinaryNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 316);
+            this.Controls.Add(this.lblBinaryNumber);
+            this.Controls.Add(this.txtBinaryNumber);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblByte);
+            this.Controls.Add(this.lblAtto);
+            this.Controls.Add(this.lblPB);
+            this.Controls.Add(this.lblPico);
+            this.Controls.Add(this.lblGB);
+            this.Controls.Add(this.lblMicro);
+            this.Controls.Add(this.lblKB);
+            this.Controls.Add(this.lblFemto);
+            this.Controls.Add(this.lblTB);
+            this.Controls.Add(this.lblNano);
+            this.Controls.Add(this.lblMB);
+            this.Controls.Add(this.txtAs);
+            this.Controls.Add(this.txtPb);
+            this.Controls.Add(this.txtFs);
+            this.Controls.Add(this.txtTb);
+            this.Controls.Add(this.txtPs);
+            this.Controls.Add(this.txtGb);
+            this.Controls.Add(this.txtNs);
+            this.Controls.Add(this.txtMb);
+            this.Controls.Add(this.txtUs);
+            this.Controls.Add(this.txtKb);
+            this.Controls.Add(this.txtMs);
+            this.Controls.Add(this.lblMill);
+            this.Controls.Add(this.txtBit);
+            this.Controls.Add(this.lblBit);
+            this.Controls.Add(this.txtByte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtByte;
+        private System.Windows.Forms.Label lblBit;
+        private System.Windows.Forms.TextBox txtBit;
+        private System.Windows.Forms.Label lblKB;
+        private System.Windows.Forms.Label lblMB;
+        private System.Windows.Forms.Label lblGB;
+        private System.Windows.Forms.Label lblTB;
+        private System.Windows.Forms.Label lblPB;
+        private System.Windows.Forms.Label lblByte;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.TextBox txtSecond;
+        private System.Windows.Forms.TextBox txtKb;
+        private System.Windows.Forms.TextBox txtMb;
+        private System.Windows.Forms.TextBox txtGb;
+        private System.Windows.Forms.TextBox txtTb;
+        private System.Windows.Forms.TextBox txtPb;
+        private System.Windows.Forms.Label lblMill;
+        private System.Windows.Forms.TextBox txtMs;
+        private System.Windows.Forms.TextBox txtUs;
+        private System.Windows.Forms.TextBox txtNs;
+        private System.Windows.Forms.TextBox txtPs;
+        private System.Windows.Forms.TextBox txtFs;
+        private System.Windows.Forms.TextBox txtAs;
+        private System.Windows.Forms.Label lblNano;
+        private System.Windows.Forms.Label lblFemto;
+        private System.Windows.Forms.Label lblMicro;
+        private System.Windows.Forms.Label lblPico;
+        private System.Windows.Forms.Label lblAtto;
+        private System.Windows.Forms.TextBox txtBinaryNumber;
+        private System.Windows.Forms.Label lblBinaryNumber;
+    }
+}
+
---
+++ UnitConvert/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+namespace UnitConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Convert Calculating";
+        }
+
+        private void txtByte_TextChanged(object sender, EventArgs e)
+        {
+            const int oneByte = 1024;
+
+            if(txtByte.Text=="")
+            {
+                txtBit.Text = "";
+                txtKb.Text = "";
+                txtMb.Text = "";
+                txtGb.Text = "";
+                txtTb.Text = "";
+                txtPb.Text = "";
+            }
+            try
+            {
+                txtBit.Text = Convert.ToString(double.Parse(txtByte.Text) * 8);
+                txtKb.Text = Convert.ToString(double.Parse(txtByte.Text) / oneByte);
+                txtMb.Text = Convert.ToString(double.Parse(txtByte.Text) / Math.Pow(oneByte, 3));
+                txtGb.Text = Convert.ToString(double.Parse(txtByte.Text) / Math.Pow(oneByte, 6));
+                txtTb.Text = Convert.ToString(double.Parse(txtByte.Text) / Math.Pow(oneByte, 9));
+                txtPb.Text = Convert.ToString(double.Parse(txtByte.Text) / Math.Pow(oneByte, 12));
+                txtBinaryNumber.Text = Convert.ToString(long.Parse(txtByte.Text), 2);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void txtSecond_TextChanged(object sender, EventArgs e)
+        {
+            const int oneThousand = 1000;
+            if(txtSecond.Text=="")
+            {
+                txtMs.Text = "";
+                txtUs.Text = "";
+                txtNs.Text = "";
+                txtPs.Text = "";
+                txtFs.Text = "";
+                txtAs.Text = "";
+                txtBinaryNumber.Text = "";
+            }
+            try
+            {
+                txtMs.Text = Convert.ToString(double.Parse(txtSecond.Text) / Math.Pow(oneThousand, 3));
+                txtUs.Text = Convert.ToString(double.Parse(txtSecond.Text) / Math.Pow(oneThousand, 6));
+                txtNs.Text = Convert.ToString(double.Parse(txtSecond.Text) / Math.Pow(oneThousand, 9));
+                txtPs.Text = Convert.ToString(double.Parse(txtSecond.Text) / Math.Pow(oneThousand, 12));
+                txtFs.Text = Convert.ToString(double.Parse(txtSecond.Text) / Math.Pow(oneThousand, 15));
+                txtAs.Text = Convert.ToString(double.Parse(txtSecond.Text) / Math.Pow(oneThousand, 18)); 
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
